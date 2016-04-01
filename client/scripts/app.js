000246b9bd2998f4f7b3de94b3009e69f965b0ea
selfresonator <selfresonator@gmail.com>
2016-04-01 15:30:22 -0700

    Appending title to DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,44 @@
-var App = React.createClass({
+SC.initialize({
+  client_id: '6538538c1dce7251c50d3ac3109bcc29'
+});
+
+var SearchSoundCloud = React.createClass({
   render: function() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <h4>Searching</h4>
+    )
+  }
+});
+
+var TrackSelector = React.createClass({
+  render: function() {
+    return (
+      <h4>Searching</h4>
+    )
+  }
+});
+
+var JukeboxApp = React.createClass({
+  render: function() {
+    return (
+      <section>
+        <header className="container text-center">
+          <h1>AJPlayer</h1>
+        </header>
+        <article className="container text-center">
+          <SearchSoundCloud />
+          <TrackSelector />
+        </article>
+        <footer className="fixed-footer">
+          Footer
+        </footer>
+      </section>
     )
   }
 });
 
 
-ReactDOM.render( < App / > ,
-  document.getElementById('num1')
+ReactDOM.render(
+  <JukeboxApp / > ,
+  document.getElementById('App')
 );
