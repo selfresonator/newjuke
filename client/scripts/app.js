18640e29493ce0c24f458c0b1a50cd9ff03f63c6
selfresonator <selfresonator@gmail.com>
2016-04-01 19:57:51 -0700

    ready to fetch tracks and create playlists

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,67 @@
+// Initialize Client ID for API call
 SC.initialize({
   client_id: '6538538c1dce7251c50d3ac3109bcc29'
 });
 
+/*
+================================================================================
+=========================STAND ALONE COMPONENTS=================================
+================================================================================
+*/
+
+var Title = React.createClass({
+  render: function(){
+    return (
+      <h1>NewJuke!</h1>
+    );
+  }
+});
+
+
+var GetUserName = React.createClass({
+  getInitialState: function() {
+    return {userName: prompt('What is your username?', 'Enter user name here')};
+  },
+
+  render: function(){
+    return (
+        <h3>Hello {this.state.userName}! What are you in the mood to hear?</h3>
+    );
+  }
+});
+
+/*
+================================================================================
+===========================MASTER AND DEPENDENTS================================
+================================================================================
+*/
 var SearchSoundCloud = React.createClass({
+  getInitialState: function() {
+    return {
+      query: ''
+    }
+  },
+
+  updateQuery: function(event) {
+    this.setState({
+      query: event.target.value
+    });
+  },
+
   render: function() {
     return (
-      <h4>Searching</h4>
+      <div className="row">
+        <div className="form-group has-feedback">
+          <input name="query"
+            type="text"
+            placeholder="Find Tracks!"
+            className = "form-control"
+            value = { this.state.query }
+            onChange = { this.updateQuery }
+            autoComplete="false"/>
+            <span></span>
+        </div>
+      </div>
     )
   }
 });
@@ -13,7 +69,7 @@ var SearchSoundCloud = React.createClass({
 var TrackSelector = React.createClass({
   render: function() {
     return (
-      <h4>Searching</h4>
+      <h4>Track selector</h4>
     )
   }
 });
@@ -23,7 +79,10 @@ var JukeboxApp = React.createClass({
     return (
       <section>
         <header className="container text-center">
-          <h1>AJPlayer</h1>
+          <Title />
+          <div id="username">
+            <GetUserName />
+          </div>
         </header>
         <article className="container text-center">
           <SearchSoundCloud />
@@ -37,7 +96,11 @@ var JukeboxApp = React.createClass({
   }
 });
 
-
+/*
+==============================
+============RENDER============
+==============================
+*/
 ReactDOM.render(
   <JukeboxApp / > ,
   document.getElementById('App')
